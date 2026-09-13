1993dbfaaf8f14d2ccb2e59154ccb94da57fb6a8
RomanRosenthal <jamielrosenthal@gmail.com>
2026-09-13 15:53:01 -0500

    Add files via upload

---
+++ print("My Daily Routine").py
@@ -0,0 +1,13 @@
+print("My Daily Routine")
+# Step 1: Waking up
+print("I wake up at 7 AM.")
+
+# Step 2: Going to school
+print("I bike to school at 7:30 AM.")
+
+# Step 3: Playing before dinner
+print("I play with my friends in the evening.")
+
+# Step 4: video game time
+print("I can play video games for 1 hour and 30 minutes")
+print("Total study hours video games for weekdays:", 1.5 * 5)
